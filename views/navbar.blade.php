f8d35879980ca50c0da95b684061805d22301b0b
karamokoba <75482401+karamokoba@users.noreply.github.com>
2022-01-25 23:17:17 +0800

    Update navbar.blade.php

--- views/navbar.blade.php
+++ views/navbar.blade.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="navbar.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="navbar">
+        <a class="active" href="#">Student</a>
+        <a href="#">Librarian</a>
+    </div>
+</body>
+</html>
