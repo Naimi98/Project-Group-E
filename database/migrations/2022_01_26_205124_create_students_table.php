e06f6fd235382fabf240c19324b4ee6b8334117a
syazanisazali <61734975+syazanisazali@users.noreply.github.com>
2022-01-27 07:22:01 +0800

    Add files via upload

---
+++ database/migrations/2022_01_26_205124_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->number('matric', 10);
+            $table->string('name', 255);
+            $table->string('email', 255);
+            $table->string('kulliyah', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
