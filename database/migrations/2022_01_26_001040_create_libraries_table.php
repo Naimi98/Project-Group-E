8a04e3fcb01c11e39c170753115a42f4914c190a
imranrahman98 <imranrahman0425@gmail.com>
2022-01-26 08:17:43 +0800

    Add files via upload

---
+++ database/migrations/2022_01_26_001040_create_libraries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('isbn');
+            $table->string('rack');
+            $table->string('quality');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libraries');
+    }
+}
