961d1f3da5b698e7dc78f439544636a925370ac5
imranrahman98 <imranrahman0425@gmail.com>
2022-01-27 12:35:26 +0800

    Update library.blade.php

--- resources/views/Librarian/library.blade.php
+++ resources/views/Librarian/library.blade.php
@@ -1,150 +1,131 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
+<head>
+  <title>Create Student</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Bootstrap CSS-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOul">
-        <link rel="stylesheet" href=" {{asset('css/style.css')}}">
-
-
-        <title>LIBRARY MANAGEMENT</title>
-    </head>
-
-    <body>
-
-    <!-- navbar-->
-
-    @if ($layout == 'index')
-        <div class="container-fluid mt-4">
-            <div class="container-fluid mt-4">
-            <div class="row justify-content-center">
-                <section class="col-md-8">
-                    @include("studentList")
-                </section>
-
-                <section class="col"></section>
-            </div>
-            </div>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <body class="font-sans antialiased">
+  <div class="min-h-screen bg-gray-100">
+  @include('librarian.layouts.navigation')
+    <!-- Page Heading -->
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Library Management System | Student') }}
+            </h2>
         </div>
+    </header>
 
-    @elseif ($layout == 'create')
-        <div class="container-fluid mt-4" id="create-form">
-            <div class="row">
-                <section class="col-md-7">
-                    @include("studentList")
-                </section>
+    <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 bg-white border-b border-gray-200">
 
-                <section class="col-md-5">
-                    <div class="card mb-3">
-                        <img src="https://wwww.timeshighereducation.com/sites/default/files/student_in_university_library.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Enter your informations below : </h5>
-                            <form action="{{ url('/store') }}" method="post">
+</head>
+<body>
 
-                                @csrf
-                                <div class="form-group">
-                                    <label>Book Name</label>
-                                    <input type="text" name="bookname" class="form-control" placeholder="Book Name:">
-                                </div><br>
+    <style>
+        body {
+          font-family: Arial, Helvetica, sans-serif;
+          background-color: black;
+        }
 
-                                <div class="form-group">
-                                    <label>Publisher Name</label><br>
-                                    <input type="text" name="publisher" class="form-control" placeholder="Publisher Name:">
-                                </div><br>
+        * {
+          box-sizing: border-box;
+        }
 
-                                <!-- sambung di title -->
-                                <div class="form-group">
-                                    <label>Location</label>
-                                    <input type="text" name="location" class="form-control"  placeholder="Location:">
-                                </div><br>
+        /* Add padding to containers */
+        .container {
+          padding: 160px;
+          background-color: white;
+        }
 
-                                <div class="form-group">
-                                    <label>Category</label>
-                                    <input type="text" name="category" class="form-control" placeholder="Category:">
-                                </div><br>
+        /* Full-width input fields */
+        input[type=text], input[type=password], input[type=number] {
+          width: 100%;
+          padding: 15px;
+          margin: 5px 0 22px 0;
+          display: inline-block;
+          border: none;
+          background: #f1f1f1;
+        }
 
-                                <div class="form-group">
-                                    <label>Book Quantity</label>
-                                    <input type="number" name="quantity" class="form-control" placeholder="Book Quantity:">
-                                </div><br>
+        input[type=text]:focus, input[type=password]:focus, input[type=number]:focus {
+          background-color: #ddd;
+          outline: none;
+        }
 
-                                <input type="submit" class="btn btn-info" value="Save">
-                                <input type="reset" class="btn btn-warning" value="Reset">
+        /* Set a style for the submit button */
+        .submit-btn {
+          background-color: #04AA6D;
+          color: white;
+          padding: 16px 20px;
+          margin: 8px 0;
+          border: none;
+          cursor: pointer;
+          width: 100%;
+          opacity: 0.9;
+        }
 
-                            </form>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
+        .submit-btn:hover {
+          opacity: 1;
+        }
 
-    @elseif ($layout == 'show')
-        <div class="container-fluid mt-4">
-            <div class="row">
-                <section class="col">
-                    @include("studentList")
-                </section>
+        /* Add a blue text color to links */
+        a {
+          color: dodgerblue;
+        }
 
-                <section class="col"></section>
-            </div>
-        </div>
+        </style>
 
-    @elseif ($layout == 'edit')
-        <div class="container-fluid mt-4">
-            <div class="row">
-                <section class="col-md-7">
-                    @include("studentList")
-                </section>
 
-                <section class="col-md-5">
-                    <div class="card mb-3">
-                        <img src="https://wwww.timeshighereducation.com/sites/default/files/student_in_university_library.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Enter the information below : </h5>
-                            <form action="{{ url('/update/'.$student->id) }}" method="post">
+<div class="container">
+  <h2 class="text-center">Add new Student</h2>
+  <br>
+  <form action = "/store" method = "post" class="form-group" style="width:70%; margin-left:15%;" action="/action_page.php">
 
-                                @csrf
-                                <div class="form-group">
-                                    <label>Book Name</label>
-                                    <input value="{{ $library->bookname }}" name="bookname" type="text" class="form-control" placeholder="Book Name">
-                                </div><br>
+  <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>"><input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
 
-                                <div class="form-group">
-                                    <label>Publisher Name</label><br>
-                                    <input value="{{ $library->publisher }}"type="text" name="publisher" class="form-control" placeholder="Publisher Name">
-                                </div><br>
+    <div class="form-group">
+        <label>Book Name</label>
+        <input type="text" name="bookname" class="form-control" placeholder="Book Name:">
+    </div><br>
 
-                                <!-- sambung di title -->
-                                <div class="form-group">
-                                    <label>Location</label>
-                                    <input value="{{ $library->location }}" type="text" name="location" class="form-control"  placeholder="Location">
-                                </div><br>
+    <div class="form-group">
+        <label>Publisher Name</label><br>
+        <input type="text" name="publisher" class="form-control" placeholder="Publisher Name:">
+    </div><br>
+    
+    <div class="form-group">
+        <label>Location</label>
+        <input type="text" name="location" class="form-control"  placeholder="Location:">
+    </div><br>
 
-                                <div class="form-group">
-                                    <label>Category</label>
-                                    <input value="{{ $library->category }}" type="text" name="category" class="form-control" placeholder="Category">
-                                </div><br>
+    <div class="form-group">
+        <label>Category</label>
+        <input type="text" name="category" class="form-control" placeholder="Category:">
+    </div><br>
 
-                                <div class="form-group">
-                                    <label>Book Quantity</label>
-                                    <input value="{{ $library->quantity }}" type="number" name="quantity" class="form-control" placeholder="Book Quantity">
-                                </div><br>
+    <div class="form-group">
+        <label>Book Quantity</label>
+        <input type="number" name="quantity" class="form-control" placeholder="Book Quantity:">
+    </div><br>
 
-                                <input type="submit" class="btn btn-info" value="Update">
-                                <input type="reset" class="btn btn-warning" value="Reset">
-
-                            </form>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-
-    @endif
+    <button type="submit" value = "Add Student" class="submit-btn">Submit</button>
+  </form>
+</div>
+ 
+ @endif
     <footer></footer>
 
     <script src="https://code.jquery.com./jquery-3.3.1.slim.min.js"> </script>
